799bf6c90a4224d87a094ba2a242e192c29584cf
aris <aristarchos.mavridis@manchester.ac.uk>
2024-11-14 12:53:51 +0000

    tweaks

--- Singer.jl
+++ Singer.jl
@@ -13,35 +13,37 @@ nhydrogens::Int32 = 2 * natoms / 3
 totalsteps = (floor(Int, num_lines / (natoms + 9)) ) 
 
 # preallocate  t (time array), and convert to seconds from fs
-t = collect(Float32, 0:totalsteps-1) .* (100 * timestep) .* 1e-15
+t = collect(Float32, 0:totalsteps-1) .* (100 * timestep) .* 1e-15;
 
 prefactor = (3 / 16) * (μ₀/(4π))^2 * ħ^2 * γ^4
-
-# Intramolecular contributions
-F = calculateF(dumpfilepath, "intra", totalsteps)
-G = prefactor * mean(ACF.(eachrow(F))) * 1e60 ;
-T1r = 1/ (J(ω₀) + 4J(2ω₀))
-
-τ_R = (1 / G[1]) * trapz(t, G) # result is 2.713696293747177e-12
-Δω²_R = 3 * prefactor * mean(mean(F.^2, dims=2)) * 10^60 
-Δω_R = sqrt(3 * prefactor * mean(mean(F.^2, dims=2)) * 10^60 )/2π
-T = 1 / (10 / 3 * Δω²_R * τ_R )
 
 J(ω) = 2 * trapz(t, (G .* cos.(ω .* t)))
 ω₀ = γ * 1 # (rad s^-1)
 
-ω₀ = 2 * 2π * 1e6
-
-T1r = 1/ (J(ω₀) + 4J(2ω₀))
-T2r = 1/ ((3/2)*J(0) + (5/2)*J(ω₀) + J(2ω₀))
-
+# Intramolecular contributions
+F = calculateF(dumpfilepath, "intra", totalsteps);
+G_R_ens_av = mean(ACF.(eachrow(F)))
+G_R = prefactor * G_R_ens_av * 1e60 ;
+τ_R = (1 / G[1]) * trapz(t, G_R) # result is 2.713696293747177e-12
+Δω²_R = 3 * prefactor * mean(mean(F.^2, dims=2)) * 1e60
+Δω_R = sqrt(Δω²_R)/2π /1000 # KHz
+Tr = 1 / (10 / 3 * Δω²_R * τ_R )
 
 # intermolecular contributions
-F = calculateF(dumpfilepath, "inter",totalsteps)
-G = prefactor * mean(ACF.(eachrow(F))) * 10^60
-τ_T = (1 / G[1]) * trapz(t, G) # result is 5.6951838649564116e-12 
-Δω²_T = 3 * prefactor * mean(mean(F.^2, dims=2)) * 10^60
+F = calculateF(dumpfilepath, "inter",totalsteps);
+G_T_ens_av = mean(ACF.(eachrow(F)))
+G_T = prefactor * G_T_ens_av * 1e60 ;
+τ_T = (1 / G[1]) * trapz(t, G_T) # result is 2.713696293747177e-12
+Δω²_T = 3 * prefactor * mean(mean(F.^2, dims=2)) * 1e60
+Δω_T = sqrt(Δω²_T)/2π /1000 # KHz
+Tt = 1 / (10 / 3 * Δω²_T * τ_T )
 
 # Relaxation times
-T = 1 / (10 / 3 * Δω²_R * τ_R + 10 / 3 * Δω²_T * τ_T)
+T = 1 / ((10 / 3) * Δω²_R * τ_R + (10 / 3) * Δω²_T * τ_T)
+
+
+T1 = 1/ (J(ω₀) + 4J(2ω₀))
+T2 = 1/ ((3/2)*J(0) + (5/2)*J(ω₀) + J(2ω₀))
+
+plot(t, G_T_ens_av, xlabel = "time (s)", label = "Intermolecular correlation function, no prefactors")
 
--- functions.jl
+++ functions.jl
@@ -11,26 +11,6 @@ const γ = 267.52218744e6; # (rad s^-1 T^-1)
 const ħ = 1.054571817e-34;  # (J s)
 const μ₀ = 1.25663706212e-6; #N A^-2
 
-# dumpfilepath = "/lustre03/other/9847zg/dump_files/dump.lammpstrj"
-
-# nblocks = 3
-# timestep = 3 #femptosec
-
-# # Count lines in dump
-# num_lines::Int32 = countlines(dumpfilepath)
-
-# # Read how many atoms are in the simulation
-# natoms = CSV.File(dumpfilepath, skipto=4, limit=1, header=false).Column1[1]
-# nhydrogens::Int32 = 2 * natoms / 3
-
-# # How many configurations are in dump (how many time steps)
-# # Modify nsteps so that " nsteps%nblocks=0 " (remove the remainder)
-# totalsteps = (floor(Int, num_lines / (natoms + 9)) ÷ nblocks) * nblocks
-
-
-# # Preallocate  t (time array)
-# t = collect(Float32, 0:totalsteps-1) .* (CSV.File(dumpfilepath, skipto=(11 + natoms), limit=1, header=false).Column1 * timestep)
-
 
 function getpairs!(Hpairs::Vector{<:AbstractVector{<:Real}}, positions::Vector{<:AbstractVector{<:Real}}, combinations)
     counter::Int32 = 1
@@ -57,7 +37,6 @@ function getpairs!(Hpairs::Vector{<:AbstractVector{<:Real}}, positions::Vector{<
                 counter += 1
             end
         end
-
     end
 end
 
@@ -74,8 +53,6 @@ function periodicboundary!(xyz::Vector{<:AbstractVector{<:Real}}, box::Vector{<:
     end
 end
 
-
-
 function cart2sph!(rtp::Vector{<:AbstractVector{<:Real}}, xyz::Vector{<:AbstractVector{<:Real}})
     for i in eachindex(rtp)
         rtp[i][1] = norm(xyz[i])
@@ -85,91 +62,12 @@ function cart2sph!(rtp::Vector{<:AbstractVector{<:Real}}, xyz::Vector{<:Abstract
 end
 
 
-
 function F012!(F::Vector{<:AbstractVector{<:Complex}}, rtp::Vector{<:AbstractVector{<:Real}})
     for i in 1:length(rtp)
         F[i][1] = complex((3 * cos(rtp[i][2])^2 - 1) / (rtp[i][1]^3))
         F[i][2] = (sin(rtp[i][2]) * cos(rtp[i][2]) * exp(-1im * rtp[i][3])) / rtp[i][1]^3
         F[i][3] = (sin(rtp[i][2])^2 * exp(-2im * rtp[i][3])) / rtp[i][1]^3
     end
-end
-
-
-
-
-function mainloop(dumpfilepath, steps, blocks, combinations)
-
-    if combinations == "all"
-        npairs = floor(Int, nhydrogens * (nhydrogens - 1) / 2)
-    elseif combinations == "inter"
-        npairs = floor(Int, nhydrogens * (nhydrogens - 1) / 2) - nhydrogens / 2
-    elseif combinations == "intra"
-        npairs = nhydrogens / 2
-    end
-
-    # Initialise arrays
-    boxlengths::Vector{Float32} = zeros(3)
-    positions::Vector{Vector{Float32}} = [zeros(3) for _ in 1:nhydrogens]
-    Hpairs::Vector{Vector{Float32}} = [[1.0, 1.0, 1.0] for _ in 1:npairs]
-    rtp::Vector{Vector{Float32}} = [[1.0, 1.0, 1.0] for _ in 1:npairs]
-    F::Vector{Vector{ComplexF32}} = [ones(ComplexF32, 3) for _ in 1:npairs]
-    F0::Vector{Vector{ComplexF32}} = [ones(ComplexF32, 3) for _ in 1:npairs]
-    G::Vector{Vector{ComplexF32}} = [ones(ComplexF32, 3) for _ in 1:steps]
-
-    # Open the dump file
-    open(dumpfilepath) do io
-
-        # Loop over time steps
-        for s in 1:steps
-
-            # Print progress (optional)
-
-            if s in floor.(Int, collect((steps/10):(steps/10):steps))
-                progresspercent = round(s * 100 / steps, digits=2)
-                display("Calculation progress: $progresspercent %")
-            end
-
-            # Skip headers
-            for _ in 1:5
-                readline(io)
-            end
-
-            # Read box bounds
-            for i in 1:3
-                boxlengths[i] = sum(abs.(parse.(Float32, split(readline(io), ' '))))
-            end
-
-            # Skip header
-            readline(io)
-
-            # Read positions
-            for i in 1:2:nhydrogens
-                readline(io) # skip the oxygen
-                positions[i] = parse.(Float32, split(readline(io), ' '))[3:5]
-                positions[i+1] = parse.(Float32, split(readline(io), ' '))[3:5]
-            end
-
-            # Do calculations
-            getpairs!(Hpairs, positions, combinations)
-            periodicboundary!(Hpairs, boxlengths)
-            cart2sph!(rtp, Hpairs)
-            F012!(F, rtp)
-
-            if isinteger((s - 1) / (steps / blocks)) # this number will only be an integer at the beginning of each block
-                F0[:] = deepcopy(F)
-            end
-
-            G[s][:] .= @views vec(mean(Base.stack(F0) .* Base.stack(conj.(F)), dims=2))
-
-            # Go to next timestep
-        end
-
-        # Close the IO file
-    end
-
-    return G
-
-    # Exit the function
 end
 
 
@@ -229,6 +127,7 @@ function calculateF(dumpfilepath,contributions,totalsteps)
 
 
             for (i, j) in enumerate(rtp)
+
                 F[i, s] = (3 * cos(j[2])^2 - 1) / j[1]^3
 
             end
@@ -412,16 +311,6 @@ function calc_msd(dumpfilepath, steps)
 
     println("The diffusion coefficient is $D m^2s^-1")
     return D
-end
-
-
-function plotG(Gmean)
-    a = plot(t[1:size(Gmean)[2]], real.(Gmean[1, :]), label="G0")
-    a = plot!(t[1:size(Gmean)[2]], real.(Gmean[2, :]), label="G1")
-    a = plot!(t[1:size(Gmean)[2]], real.(Gmean[3, :]), label="G2")
-    # plot!(background_color=:transparent)
-    a = plot!(xaxis="time (fs)", yaxis="G (Angstrom^-6)")
-    display(a)
 end
 
 
